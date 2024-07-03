5012c98b78af7658d25a4f0e6f2404535a15a1db
Liu <Fei.Liu3@harman.com>
2024-07-03 15:35:08 +0800

    Initial Commit

---
+++ src/lib/components/layout/Sidebar.svelte
@@ -0,0 +1,15 @@
+
+
+<div>
+</div>
+
+<style>
+	.scrollbar-hidden:active::-webkit-scrollbar-thumb,
+	.scrollbar-hidden:focus::-webkit-scrollbar-thumb,
+	.scrollbar-hidden:hover::-webkit-scrollbar-thumb {
+		visibility: visible;
+	}
+	.scrollbar-hidden::-webkit-scrollbar-thumb {
+		visibility: hidden;
+	}
+</style>
